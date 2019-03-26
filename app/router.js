84a5def320855d91162d86fd074ff25419a62837
Marc Lynch <lynchmarc2@gmail.com>
2019-03-26 13:04:50 -0700

    basic tracerbench instrumentation

--- app/router.js
+++ app/router.js
@@ -1,9 +1,29 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import {
+  schedule
+} from '@ember/runloop';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+  init() {
+    this._super(...arguments);
+
+    this.on('routeDidChange', transition => {
+      schedule("afterRender", () => {
+        requestAnimationFrame(function () {
+          if (location.search === "?tracing") {
+            requestAnimationFrame(function () {
+              setTimeout(function () {
+                document.location.href = "about:blank";
+              }, 0);
+            });
+          }
+        });
+      });
+    });
+  }
 });
 
 Router.map(function () {

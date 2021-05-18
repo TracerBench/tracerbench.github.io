390292af24a3499d47ee23653d083c82f4643c69
Marc Lynch <lynchmarc2@gmail.com>
2021-05-18 10:29:39 -0700

    chore: bump deps to latest

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,3 +1,5 @@
+/* eslint-disable ember/require-tagless-components */
+/* eslint-disable ember/no-classic-components */
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 
